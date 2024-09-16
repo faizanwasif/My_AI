d4ded30d771f0f00d9c0a8b1a329a725c5c5f569
faizanwasif <faizanwasif102@gmail.com>
2024-09-16 16:09:37 +0500

    Merged remote changes with local modifications

--- src/models/web_crawler_model.py
+++ src/models/web_crawler_model.py
@@ -15,7 +15,11 @@ class WebCrawlerModel:
     def _create_web_crawl_agent(self):
         return Agent(
             role="Researcher",
+<<<<<<< Updated upstream
             goal="""Find relevant sources like articles, blogs and research papers on the topic: {info} """,
+=======
+            goal="Find relevant sources like articles, blogs and research papers on the topic: {info}. Use DuckDuckGo to search for the most relevant articles, blogs, or sources.",
+>>>>>>> Stashed changes
             backstory="""
                 You are a PhD research assistant at a prestigious university.
                 Your primary role involves conducting thorough searches for relevant academic articles, research papers, and scholarly works to support your advisor and research team. 
@@ -23,7 +27,11 @@ class WebCrawlerModel:
                 Your work is crucial in aiding the research team’s direction and contributing to academic publications.
             """,
             max_iter=20,
+<<<<<<< Updated upstream
             tools=[self.url_tool],
+=======
+            tools=[self.search_tool],
+>>>>>>> Stashed changes
             llm=self.model,
             allow_delegation=True,
             verbose=True
@@ -35,6 +43,7 @@ class WebCrawlerModel:
             
             description="""
 
+<<<<<<< Updated upstream
                 Extract key-words from {info}.
                
                 Search for URls related to those keywords by adding a + symbol between all the keywords.
@@ -52,6 +61,16 @@ class WebCrawlerModel:
                
               
                 Provide a list URLs of most relevant articles.
+=======
+                Search for articles related to "{info}" using "https://www.google.com". 
+                The articles should be the newest and most relevant based on the topic provided.
+
+            """,
+            expected_output="""
+                Provide exact title of the research papers and articles that are the most relevant to the topic provided.
+                Provide their summery as well.
+                Provide a list of only 10 most relevant articles and their summery.
+>>>>>>> Stashed changes
             """,
             tools=[self.url_tool],
             agent=self.web_crawl_agent
@@ -68,6 +87,7 @@ class WebCrawlerModel:
     def process_input(self, user_input):
         domain_input = {"info": user_input}
         return self.crew.kickoff(inputs=domain_input)
+<<<<<<< Updated upstream
 
       
 #     Task(
@@ -87,3 +107,6 @@ class WebCrawlerModel:
 #     tools=[self.search_tool],
 #     agent=self.web_crawl_agent
 # )
+=======
+    
+>>>>>>> Stashed changes
